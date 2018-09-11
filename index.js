25c86360d0edd67cad434f9e7d836f0ebb9f6c93
Kevin Kim <kevintaekim80@gmail.com>
2018-09-11 14:14:23 -0700

    update base index.js file by importing and doing basic testing

--- index.js
+++ index.js
@@ -1,72 +1,40 @@
-const puppeteer = require('puppeteer');
+// const db = require('./db/index.js');
+// const ShoeModel = require('./model/shoe.js');
+const FamousFootwear = require('./lib/famousfootwear');
 
-
-let sweep = async () => {
-	const browser = await puppeteer.launch({headless: false}); 
-	const page = await browser.newPage();	
-	await page.setViewport({ width: 1280, height: 800 });
-
-	
-	await page.goto('https://shop.nordstrom.com/c/sale-mens-sneakers?breadcrumb=Home%2FSale%2FMen%2FShoes%2FSneakers%20%26%20Athletic', {waitUntil: 'networkidle2'});
-
-	await page.waitForSelector('[data-element="touch-target product-module-media-link"]');
-
-	let baseURL = await page.evaluate(() => {
-		let protocol = window.location.protocol;
-		let host = window.location.host;
-		return `${protocol}//${host}`;
-	});
-
-	await console.log(baseURL);	
-
-	let itemLinks = await page.$$eval('[data-element="touch-target product-module-media-link"]', (elems) => {
-		return elems.map((el) => {
-			let href = el.getAttribute('href');
-			return href;
-		});
-	});
-
-	await Promise.all([
-	  page.waitForNavigation({waitUntil: 'load'}),
-	  page.click('[data-element="page-number-2"]')
-	]);	
-
-	await page.waitForSelector('[data-element="touch-target product-module-media-link"]');
-
-	console.log('page changed to 2');
-
-	await page.waitFor(3000)
-
-	let itemLinks2 = await page.$$eval('[data-element="touch-target product-module-media-link"]', (elems) => {
-		return elems.map((el) => {
-			let href = el.getAttribute('href');
-			return href;
-		});
-	});	
-
-	await Promise.all([
-	  page.waitForNavigation({waitUntil: 'load'}),
-	  page.click('[data-element="page-number-3"]')
-	]);	
-
-	await page.waitForSelector('[data-element="touch-target product-module-media-link"]');	
-
-	console.log('page changed to 3')
-
-	await page.waitFor(3000)
-
-	let itemLinks3 = await page.$$eval('[data-element="touch-target product-module-media-link"]', (elems) => {
-		return elems.map((el) => {
-			let href = el.getAttribute('href');
-			return href;
-		});
-	});	
-
-	await console.log(itemLinks3);
-
+var page1 = {
+	url: 'https://www.famousfootwear.com/en-US/Mens/_/_/Athletic+Shoes/On+Sale/Products.aspx',
+	category: 'Sandals',
+	site: 'Finishline'
 };
 
-sweep()
-.catch((err) => {
-	console.log(err);
-}) ;
+var page2 = {
+	url: 'https://www.famousfootwear.com/en-US/Mens/_/_/Athletic+Shoes~Fashion+Sneakers/On+Sale/Products.aspx',
+	category: 'Comfort',
+	site: 'Finishline'
+};
+
+var pageList = [page2];
+
+let getItemsFromPages = async (pageArr) => {
+	let itemCollection = [];
+	for (let page of pageArr) {
+		let defaultProps = {
+			category: page.category,
+			site: page.site,
+		};
+		let items = await FamousFootwear.itemListSweep.staticSweep(page.url, defaultProps);
+		itemCollection = [...itemCollection, ...items];
+	}
+	return itemCollection;	
+}
+
+let getItemDatafromCollection = async (itemCollection) => {
+	let itemObj = itemCollection[0];
+	let itemData = await FamousFootwear.itemDataSweep.staticSweep(itemObj);
+	console.log(itemData);
+} 
+
+
+getItemsFromPages(pageList)
+.then(getItemDatafromCollection);
